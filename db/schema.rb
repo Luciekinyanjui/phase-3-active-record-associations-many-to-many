0a3b3d884718e83eeec846829c95d046d97b0909
Luciekinyanjui <lucy.wambui@moringaschool.com>
2022-09-24 15:36:51 +0300

    Lab Complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_07_19_113221) do
+ActiveRecord::Schema.define(version: 2022_06_08_113639) do
 
   create_table "games", force: :cascade do |t|
     t.string "title"
     t.string "genre"
     t.string "platform"
     t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
